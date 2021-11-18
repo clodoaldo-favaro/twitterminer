7199adead515be3b8a019d19936776f0feeed2b6
Clodoaldo Favaro <clodoaldofavero@gmail.com>
2021-11-18 10:15:53 -0300

    Tela de gravar pesquisa.

---
+++ app/src/main/java/com/example/twitterminer/NovaPesquisaActivity.java
@@ -0,0 +1,14 @@
+package com.example.twitterminer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NovaPesquisaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova_pesquisa);
+    }
+}
