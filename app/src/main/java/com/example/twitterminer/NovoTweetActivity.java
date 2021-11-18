b59b9c72a928482786741a384ffe3171abb8076a
Clodoaldo Favaro <clodoaldofavero@gmail.com>
2021-11-18 10:23:26 -0300

    Tela novo tweet

---
+++ app/src/main/java/com/example/twitterminer/NovoTweetActivity.java
@@ -0,0 +1,14 @@
+package com.example.twitterminer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NovoTweetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_tweet);
+    }
+}
