9ca2227b6ecc93ca093fcffb3c732ca8b5846e39
Clodoaldo Favaro <clodoaldo.favero@bling.com.br>
2021-11-21 12:24:32 -0300

    Fab adicionar pesquisa; migra codigo de inclusao para tela de listagem.

--- app/src/main/java/com/example/twitterminer/ListagemPesquisasActivity.java
+++ app/src/main/java/com/example/twitterminer/ListagemPesquisasActivity.java
@@ -1,13 +1,20 @@
 package com.example.twitterminer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.twitterminer.entities.Pesquisa;
+import com.example.twitterminer.viewmodel.PesquisaViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,17 +24,52 @@ import com.example.twitterminer.databinding.ActivityListagemPesquisasBinding;
 
 public class ListagemPesquisasActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityListagemPesquisasBinding binding;
+    FloatingActionButton novaPesquisaButton;
+    public static final int NOVA_PESQUISA_ACTIVITY_REQUEST_CODE = 1;
+    private PesquisaViewModel mPesquisaViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listagem_pesquisas);
 
-        binding = ActivityListagemPesquisasBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        mPesquisaViewModel = new ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(PesquisaViewModel.class);
 
-        setSupportActionBar(binding.toolbar);
+        novaPesquisaButton = findViewById(R.id.adicionarPesquisaFab);
+
+        novaPesquisaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListagemPesquisasActivity.this, NovaPesquisaActivity.class);
+
+                startActivityForResult(intent, NOVA_PESQUISA_ACTIVITY_REQUEST_CODE);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NOVA_PESQUISA_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Pesquisa pesquisa = new Pesquisa();
+            pesquisa.titulo = data.getStringExtra(NovaPesquisaActivity.EXTRA_TITULO);
+            pesquisa.descricao = data.getStringExtra(NovaPesquisaActivity.EXTRA_DESCRICAO);
+            pesquisa.palavrasChave = data.getStringExtra(NovaPesquisaActivity.EXTRA_PALAVRAS_CHAVE);
+            pesquisa.respostasPossiveis = data.getStringExtra(NovaPesquisaActivity.EXTRA_RESPOSTAS);
+
+            mPesquisaViewModel.insert(pesquisa);
+
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Pesquisa salva com sucesso!",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Não foi possível salvar a pesquisa",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
 
--- app/src/main/java/com/example/twitterminer/TelaInicialActivity.java
+++ app/src/main/java/com/example/twitterminer/TelaInicialActivity.java
@@ -25,21 +25,22 @@ public class TelaInicialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
 
-        mPesquisaViewModel = new ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(PesquisaViewModel.class);
+        //mPesquisaViewModel = new ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(PesquisaViewModel.class);
 
         pesquisasButton = findViewById(R.id.buttonPesquisas);
 
         pesquisasButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(TelaInicialActivity.this, NovaPesquisaActivity.class);
+                Intent intent = new Intent(TelaInicialActivity.this, ListagemPesquisasActivity.class);
 
-                startActivityForResult(intent, NOVA_PESQUISA_ACTIVITY_REQUEST_CODE);
+                //startActivityForResult(intent, NOVA_PESQUISA_ACTIVITY_REQUEST_CODE);
+                startActivity(intent);
             }
         });
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    /*public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == NOVA_PESQUISA_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
@@ -61,5 +62,5 @@ public class TelaInicialActivity extends AppCompatActivity {
                     "Não foi possível salvar a pesquisa",
                     Toast.LENGTH_LONG).show();
         }
-    }
+    }*/
 }
