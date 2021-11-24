99078d7acffca768b285381030ca831f2c220e47
Clodoaldo Favaro <clodoaldofavero@gmail.com>
2021-11-24 20:55:05 -0300

    Monta query, inclui lib twitter4j no gradle.

--- app/src/main/java/com/example/twitterminer/ColetarTweetsActivity.java
+++ app/src/main/java/com/example/twitterminer/ColetarTweetsActivity.java
@@ -9,6 +9,10 @@ import android.widget.Button;
 
 import com.example.twitterminer.entities.Pesquisa;
 
+import twitter4j.Query;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
 public class ColetarTweetsActivity extends AppCompatActivity {
 
     public static final String EXTRA_ID_PESQUISA = "pesquisa.id";
@@ -16,6 +20,8 @@ public class ColetarTweetsActivity extends AppCompatActivity {
     public static final String EXTRA_RESPOSTAS = "pesquisa.respostas";
 
     private Pesquisa pesquisaTela;
+    private int idPesquisa;
+    private Twitter twitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +30,25 @@ public class ColetarTweetsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent.hasExtra(EXTRA_ID_PESQUISA)) {
+            idPesquisa = intent.getIntExtra(EXTRA_ID_PESQUISA, 0);
+            twitter = TwitterFactory.getSingleton();
+            twitter.setOAuthConsumer("gbeDAH1NxF35GJmDQLBPNE0d4", "mE8kfbyxbd1e2ecIFWcxHlIf4tl3hrCA2RvMIPh2QHpTWtYEAu");
+
+            String palavrasChave = intent.getStringExtra(EXTRA_PALAVRAS_CHAVE);
+            String[] arrayPalavras = palavrasChave.split(",");
+            String queryString = "";
+            for (int i = 0; i < arrayPalavras.length; i++) {
+                queryString += arrayPalavras[i].trim();
+                if (i < arrayPalavras.length - 1) {
+                    queryString += " OR ";
+                }
+            }
+
+            Query query = new Query(queryString);
 
         }
 
-        Button buttonParar = findViewById(R.id.buttonColetarTweets);
+        Button buttonParar = findViewById(R.id.buttonPararColeta);
 
         buttonParar.setOnClickListener(new View.OnClickListener() {
             @Override
